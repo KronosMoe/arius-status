60d0750a13230ccf77f458adef53bd8e31e4d4a5
Mirailisc <mirailisclm@gmail.com>
2025-06-09 18:42:36 +0700

    sdfsdf

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -9,7 +9,7 @@ import hooksPlugin from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default tseslint.config(
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], ignores: ['src/lib/i18n.mjs', 'node_modules'] },
   { languageOptions: { globals: globals.browser, ecmaVersion: 'latest' } },
   js.configs.recommended,
   pluginJs.configs.recommended,
