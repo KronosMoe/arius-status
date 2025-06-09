05ab7f7dbdef0758d459764992efd7b1808c040b
Mirailisc <mirailisclm@gmail.com>
2025-06-09 15:52:18 +0700

    asdasdada

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from 'cookie-parser'
 import * as bodyParser from 'body-parser'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { join } from 'path'
+import './libs/tracer'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
